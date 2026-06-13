de6f0605dbfe5a2782ff935bdaffdef314c7e471
Amruth Tetakali <156193119+amruth6002@users.noreply.github.com>
2026-06-13 04:40:17 +0000

    add

---
+++ dp/1d-dp/frog_jump.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+        int n=height.size();
+        vector<int> dp(n,0);
+        dp[0]=0;
+        dp[1]=abs(height[0]-height[1]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=min(dp[i-1]+abs(height[i]-height[i-1]),dp[i-2]+abs(height[i]-height[i-2]));
+        }
+        return dp[n-1];
+        
+    }
+};
